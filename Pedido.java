b7610fb78d65a79da0ed8dda2de23b01b3eb7b57
Sandro Lucas <124007323+Lucsxt@users.noreply.github.com>
2023-09-26 22:28:27 -0300

    Update Pedido.java

--- Pedido.java
+++ Pedido.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 
+// By Sandro Lucas...
+// For Nicole, my girlfriend!
+
 public class Pedido {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
